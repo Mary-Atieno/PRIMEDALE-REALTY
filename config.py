7558e7d86ae763431bdf387e409eb947bcfe883c
albright.human <sungutialbright@gmail.com>
2022-05-16 18:43:40 +0300

    add database configurations

---
+++ config.py
@@ -0,0 +1,38 @@
+import os
+
+class Config:
+    SECRET_KEY='secret'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://abrighthuman:bright@localhost/primedale'
+    # UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    SQALCHEMY_TRACK_MODIFICATIONS = True
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class ProdConfig(Config):
+    uri = os.getenv('DATABASE_URL')
+    if uri and uri.startswith('postgres://'):
+        uri = uri.replace('postgres://', 'postgresql://', 1)
+        
+        SQLALCHEMY_DATABASE_URI = uri
+DEBUG = True
+
+
+class DevConfig(Config):
+         SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://abrighthuman:bright@localhost/primedale'
+
+DEBUG = True
+
+config_options={
+    'development':DevConfig,
+    'production':ProdConfig
+}
